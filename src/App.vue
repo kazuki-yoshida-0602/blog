e83d4fb4b577164f59696bcea1a21a014e05ded8
YoshidaKazuki <yoshidakazuki@team-lab.com>
2018-09-17 13:20:21 +0900

    レスポンシブの実験を追加

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,29 @@
 #app
   .hero
     header.header
-      .header__title
-        router-link(class="link", to="/home")
-          | Kazuki Yoshida
-      p.header__description
-        | @Tokyo, Japan. <br />
-        | Backend web developer, Master of Physics.
-      nav.menu
-        ul.menu
-          router-link(class="menu__item", to="/home")
-            span
-              | home
-          router-link(class="menu__item", to="/about")
-            span
-              | about
-          router-link(class="menu__item", to="/blog")
-            span
-              | blog
+      transition(appear, name="title")
+        .header__title
+          router-link(class="link", to="/home")
+            p.first
+              | Kazuki
+            p.last
+              | Yoshida
+      transition(appear, name="menu")
+        p.header__description
+          | @Tokyo, Japan. <br />
+          | I work as a Backend web developer, and I'm learning Frontend and Embedded system.
+      transition(appear, name="menu")
+        nav.menu
+          ul.menu
+            router-link(class="menu__item", to="/home")
+              span
+                | home
+            router-link(class="menu__item", to="/about")
+              span
+                | about
+            router-link(class="menu__item", to="/blog")
+              span
+                | blog
 
     router-view(v-bind:post="post")
 
@@ -28,6 +34,11 @@
 </template>
 
 <style lang="scss">
+@import 'mixins';
+
+/*************************************/
+/*              design               */
+/*************************************/
 $padding-size:      20px;
 $hero-image:        "../images/2018/08/bari.jpg";
 
@@ -53,36 +64,69 @@ body {
 
 header.header {
   /*  border: solid #808000; */
+
   width: 25%;
   position: fixed;
   bottom: 10%;
   left: 6.5%;
 
-  text-shadow: 0em 0em 10em black, 0em 0em 10em black;
+  @include sp {
+  width: 88%;
+  position: fixed;
+  bottom: 20%;
+  left: 6.5%;
+  }
+
+  /* text-shadow: 0em 0em 10em black, 0em 0em 10em black; */
+  color: white;
+  text-decoration: none;
+}
+
+a:visited, a {
   color: white;
   text-decoration: none;
 
-  > a:visited, a {
-    color: white;
-    text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+}
 
-    &:hover {
-      text-decoration: underline;
-    }
+.header__title {
+  /* font-size: 32px; */
+  font-weight: bold;
+  /* border: solid black; */
+
+  @include pc {
+    font-size: 80px;
+    line-height: 82px;
+    /* border: solid blue; */
   }
 
-  > .header__title {
-    font-size: 2.7em;
-    font-weight: bold;
+  @include tb {
+    font-size: 80px;
+    line-height: 82px;
+    border: solid red;
   }
 
-  > p.header__description {
-    font-size: 1.2em;
+  @include sp {
+    font-size: 60px;
+    border: solid #808000;
   }
+}
 
-  > .menu {
-    font-size: 1.2em;
-  }
+p.header__description {
+  font-size: 16px;
+  line-height: 24px;
+}
+
+.menu {
+  /* border: solid 1px #808000; */
+  font-size: 16px;
+}
+
+ul.menu {
+  display:flex;
+  justify-content: space-around;
 }
 
 span {
@@ -99,6 +143,35 @@ footer.footer {
   color: white;
   text-align: center;
   text-shadow: 0px 0px 1em black, 0px 0px 1em black;
+
+  @include sp {
+    font-size: 6px;
+  }
+}
+
+
+/*************************************/
+/*            transition             */
+/*************************************/
+.title-enter-active, .title-leave-active {
+    transition: opacity 1s;
+}
+.title-enter, .title-leave-to {
+    opacity: 0;
+}
+
+.desc-enter-active, .desc-leave-active {
+    transition: opacity 3s;
+}
+.desc-enter, .desc-leave-to {
+    opacity: 0;
+}
+
+.menu-enter-active, .menu-leave-active {
+    transition: opacity 3s;
+}
+.menu-enter, .menu-leave-to {
+    opacity: 0;
 }
 
 </style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,108 +1,114 @@
 <template lang="pug">
 .about
-  .top-image
-    img(class="top-image__img", src="images/sheep.png")
-    img(class="top-image__img", src="images/selfie.jpg")
-  .description
-    | 都内でWebエンジニアとして働き、バックエンド・フロントエンドの修行を積む26才。<br />
-    | 事業構想の策定からプロダクト設計・開発までできるようになりたいと思っている。<br />
-    | 趣味は美味しいお酒とおつまみを用意して映画を流しながらコードを書くこと。<br />
-  .section
+  transition(appear, name="contents")
+    .top-image
+      img(class="top-image__img", src="images/sheep.png")
+      img(class="top-image__img", src="images/selfie.jpg")
+  transition(appear, name="contents2")
+    .description
+      | 都内でWebエンジニアとして働き、バックエンド・フロントエンドの修行を積む26才。<br />
+      | 作りたいものが色々とあるので時間を見つけて開発している。<br />
+      | 趣味は美味しいお酒とおつまみを用意して映画を流しながらコードを書くこと。<br />
+  transition(appear, name="contents3")
+    .section
 
-    //- スキル
-    .section__title
-      | ・Skills
-    .section__content
-      span.section__content__index
-        | Backend
-      span.section__content__value
-        | Ruby, Ruby on Rails<br />
+      //- スキル
+      .section__title
+        | ・Skills
+      .section__content
+        span.section__content__index
+          | Backend
+        span.section__content__value
+          | Ruby, Ruby on Rails<br />
+  
+        span.section__content__index
+          | Frontend
+        span.section__content__value
+          | Vue.js<br />
+  
+        span.section__content__index
+          | Others
+        span.section__content__value
+          | Microsoft Excel<br />
 
-      span.section__content__index
-        | Frontend
-      span.section__content__value
-        | Vue.js<br />
+      //- ソーシャル
+      .section__title
+        | ・Social
+      .section__content
+        span.section__content__index
+          | GitHub
+        span.section__content__value
+          a(href="https://github.com/kazuki-yoshida-0602", target="_blank")
+            | @kazuki-yoshida-0602<br />
 
-      span.section__content__index
-        | Others
-      span.section__content__value
-        | Microsoft Excel<br />
+        span.section__content__index
+          | Twitter
+        span.section__content__value
+          a(href="https://twitter.com/__qooa__", target="_blank")
+            | @__qooa__<br />
 
-    //- ソーシャル
-    .section__title
-      | ・Social
-    .section__content
-      span.section__content__index
-        | GitHub
-      span.section__content__value
-        a(href="https://github.com/kazuki-yoshida-0602", target="_blank")
-          | @kazuki-yoshida-0602<br />
+      //- 趣味
+      .section__title
+        | ・Favorites
+      .section__content
+        span.section__content__index
+          | Book
+        span.section__content__value
+          a(href="https://www.amazon.co.jp/dp/B00NQ3QONK/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1", target="_blank")
+            | Zero to One (Peter Thiel著)<br />
 
-      span.section__content__index
-        | Twitter
-      span.section__content__value
-        a(href="https://twitter.com/__qooa__", target="_blank")
-          | @__qooa__<br />
+        span.section__content__index
+          | Music
+        span.section__content__value
+          a(href="https://www.youtube.com/watch?v=q0xK_QRuP8Y", target="_blank")
+            | Bunt, 
+          a(href="https://www.youtube.com/watch?v=m-M1AtrxztU", target="_blank")
+            | Clean Bandit, 
+          a(href="https://www.youtube.com/watch?v=AYD7TuXWVH8", target="_blank")
+            | Steve Aoki,
+            |..<br />
+        span.section__content__index
+          | Movie
+        span.section__content__value
+          a(href="https://www.youtube.com/watch?v=SGWvwjZ0eDc", target="_blank")
+            | The Departed (監督: Martin Scorsese)<br />
 
-    //- 趣味
-    .section__title
-      | ・Favorites
-    .section__content
-      span.section__content__index
-        | Book
-      span.section__content__value
-        a(href="https://www.amazon.co.jp/dp/B00NQ3QONK/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1", target="_blank")
-          | Zero to One (Peter Thiel著)<br />
-
-      span.section__content__index
-        | Music
-      span.section__content__value
-        a(href="https://www.youtube.com/watch?v=q0xK_QRuP8Y", target="_blank")
-          | Bunt, 
-        a(href="https://www.youtube.com/watch?v=m-M1AtrxztU", target="_blank")
-          | Clean Bandit, 
-        a(href="https://www.youtube.com/watch?v=AYD7TuXWVH8", target="_blank")
-          | Steve Aoki,
-          |..<br />
-      span.section__content__index
-        | Movie
-      span.section__content__value
-        a(href="https://www.youtube.com/watch?v=SGWvwjZ0eDc", target="_blank")
-          | The Departed (監督: Martin Scorsese)<br />
-
-    //- 履歴
-    .section__title
-      | ・History
-    .section__content
-      span.section__content__index
-        | 2000
-      span.section__content__value
-        | 物理学者に憧れて勉強を始める<br />
-      span.section__content__index
-        | 2008
-      span.section__content__value
-        | 学芸大附高に入りプログラミングを始める<br />
-      span.section__content__index
-        | 2011
-      span.section__content__value
-        | 京都大学に入学、サークル活動や留学など<br />
-      span.section__content__index
-        | 2017
-      span.section__content__value
-        | 戦略コンサルとして経営再建、経営計画の策定に携わる<br />
-      span.section__content__index
-        | 2018
-      span.section__content__value
-        | 東京のIT企業へ入社しエンジニアに  ← Now<br />
+      //- 履歴
+      .section__title
+        | ・History
+      .section__content
+        span.section__content__index
+          | 2000
+        span.section__content__value
+          | 物理学に興味を持ち勉強を始める<br />
+        span.section__content__index
+          | 2008
+        span.section__content__value
+          | 学芸大附高に入りプログラミングを始める<br />
+        span.section__content__index
+          | 2011
+        span.section__content__value
+          | 京都大学に入学、サークル活動や留学など気ままに過ごす<br />
+        span.section__content__index
+          | 2017
+        span.section__content__value
+          | 戦略コンサルとして経営再建PJ、経営計画策定PJなどに携わる<br />
+        span.section__content__index
+          | 2018
+        span.section__content__value
+          | 東京のIT企業へ入社しWebエンジニアに  ← Now<br />
 
 </template>
 
 <style lang="scss" scoped>
+@import '../mixins';
+
 $padding-size: 20px;
 
 .about {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  font-size: 1.2em;
+  /* font-size: 1.2em; */
+  font-size: 14px;
 
   background-color: white;
   /* opacity: 0.85; */
@@ -112,9 +118,20 @@ $padding-size: 20px;
   width: 61%;
   height: 91%;
   padding: 2%;
+  box-shadow: 0px 4px 8px rgba(0,0,0,0.3);
 
   position: fixed;
   right: calc(20px + 3%);
+
+  @include sp {
+  width: 94%;
+  position: fixed;
+  height: 91%;
+  right: calc(12px);
+
+  font-size: 12px;
+  }
+
 }
 
 .top-image__img {
@@ -151,6 +168,31 @@ a:visited, a {
   &:hover {
     text-decoration: underline;
   }
+}
+
+
+/*************************************/
+/*            transition             */
+/*************************************/
+.contents-enter-active, .contents-leave-active {
+    transition: opacity 1.5s;
+}
+.contents-enter, .contents-leave-to {
+    opacity: 0;
+}
+
+.contents2-enter-active, .contents2-leave-active {
+    transition: opacity 1.5s;
+}
+.contents2-enter, .contents2-leave-to {
+    opacity: 0;
+}
+
+.contents3-enter-active, .contents3-leave-active {
+    transition: opacity 1.5s;
+}
+.contents3-enter, .contents3-leave-to {
+    opacity: 0;
 }
 
 </style>
